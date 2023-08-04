79bf42932ab5a8fc6cc71e6acc4f6599fc7da783
LuanRM1 <luan.mello@sou.inteli.edu.br>
2023-08-04 08:58:52 -0300

    wwfw

--- exercicio1.py
+++ exercicio1.py
@@ -6,13 +6,14 @@
 
 # Crie a função que será avaliada no exercício aqui
 
+def conta_palavras_unicas(texto):
+    texto = texto.split()
+    texto = set(texto)
+    return len(texto)
 
 
 
 
-
-
-# Teste a sua função aqui (caso ache necessário)
 
 
 
--- exercicio2.py
+++ exercicio2.py
@@ -6,15 +6,21 @@
 
 # Crie a função que será avaliada no exercício aqui
 
-
+def is_anagram(palavra1, palavra2):
+    if len(palavra1) != len(palavra2):
+        return False
+    palavra1 = sorted(palavra1)
+    palavra2 = sorted(palavra2)
+    if palavra1 == palavra2:
+        return True
+    else:
+        return False
 
 
 
 
 
 # Teste a sua função aqui (caso ache necessário)
-
-
 
 
 
